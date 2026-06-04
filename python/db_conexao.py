04a45f3140c5abcbdb075a2cf11099ea45a804fe
GuilhermeAnitelli <guilherme.anitelli10@gmail.com>
2026-06-04 12:39:25 -0300

    feat: implementa autenticação segura para conexão com o banco de dados e adiciona .env ao .gitignore

--- python/db_conexao.py
+++ python/db_conexao.py
@@ -1,14 +1,25 @@
 import oracledb
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 def conectar_oracle():
-    print("\nCredenciais de Acesso ao Banco de Dados")
+    print("\n--- Conectando ao Banco de Dados (Autenticação Segura) ---")
     
-    login = input('Usuário: ')
-    senha = input('Senha: ')
-
+    # Busca as credenciais diretamente do OS(Sistema Operacional) usando o python-dotenv
+    login = os.getenv("ORACLE_USER")
+    senha = os.getenv("ORACLE_PASSWORD")
+    
+    # Trava de segurança(se esquecer de criar o .env)
+    if not login or not senha:
+        print("[ERRO DE SEGURANÇA] Arquivo .env não encontrado ou incompleto.")
+        print("Crie um arquivo '.env' na raiz com ORACLE_USER e ORACLE_PASSWORD.")
+        return None
+    
     try:
         print("Estabelecendo conexão, por favor aguarde...")
-
         conn = oracledb.connect(user=login,
                                 password=senha,
                                 host="oracle.fiap.com.br",
@@ -17,7 +28,7 @@ def conectar_oracle():
         
         print("Conexão estabelecida com sucesso!")
         return conn
-    
+        
     except oracledb.DatabaseError as e:
         print(f"\n[ERRO CRÍTICO] Falha na conexão com o Oracle: {e}")
         return None
